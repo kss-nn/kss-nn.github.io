b52274d4e92c609ca9fde2a624cec97e286efbd0
kss-nn <67142065+kss-nn@users.noreply.github.com>
2021-04-03 08:42:59 +0200

    done

---
+++ app.js
@@ -0,0 +1,365 @@
+'use strict';
+
+// Update current year:
+
+document.querySelectorAll('.year').forEach(item =>
+    item.innerHTML = new Date().getFullYear());
+
+// Set MAN Lion's Coach carousel to clicked frame:
+
+if (document.querySelector('#man-lions-coach-sm-img-0')) {
+    document.querySelector('#man-lions-coach-sm-img-0')
+        .addEventListener('click', function () {
+            $('#ManLionsCoachCarouselIndicators').carousel(0);
+        })
+}
+
+if (document.querySelector('#man-lions-coach-sm-img-1')) {
+    document.querySelector('#man-lions-coach-sm-img-1')
+        .addEventListener('click', function () {
+            $('#ManLionsCoachCarouselIndicators').carousel(1);
+        })
+}
+
+if (document.querySelector('#man-lions-coach-sm-img-2')) {
+    document.querySelector('#man-lions-coach-sm-img-2')
+        .addEventListener('click', function () {
+            $('#ManLionsCoachCarouselIndicators').carousel(2);
+        })
+}
+
+if (document.querySelector('#man-lions-coach-sm-img-3')) {
+    document.querySelector('#man-lions-coach-sm-img-3')
+        .addEventListener('click', function () {
+            $('#ManLionsCoachCarouselIndicators').carousel(3);
+        })
+}
+
+if (document.querySelector('#man-lions-coach-sm-img-4')) {
+    document.querySelector('#man-lions-coach-sm-img-4')
+        .addEventListener('click', function () {
+            $('#ManLionsCoachCarouselIndicators').carousel(4);
+        })
+}
+
+if (document.querySelector('#man-lions-coach-sm-img-5')) {
+    document.querySelector('#man-lions-coach-sm-img-5')
+        .addEventListener('click', function () {
+            $('#ManLionsCoachCarouselIndicators').carousel(5);
+        })
+}
+
+if (document.querySelector('#man-lions-coach-sm-img-6')) {
+    document.querySelector('#man-lions-coach-sm-img-6')
+        .addEventListener('click', function () {
+            $('#ManLionsCoachCarouselIndicators').carousel(6);
+        })
+}
+
+if (document.querySelector('#man-lions-coach-sm-img-7')) {
+    document.querySelector('#man-lions-coach-sm-img-7')
+        .addEventListener('click', function () {
+            $('#ManLionsCoachCarouselIndicators').carousel(7);
+        })
+}
+
+// Set Mercedes Sprinter 316 CDI carousel to clicked frame:
+
+if (document.querySelector('#mercedes-sprinter-316-cdi-sm-img-0')) {
+    document.querySelector('#mercedes-sprinter-316-cdi-sm-img-0')
+        .addEventListener('click', function () {
+            $('#MercedesSprinter316CdiCarouselIndicators').carousel(0);
+        })
+}
+
+if (document.querySelector('#mercedes-sprinter-316-cdi-sm-img-1')) {
+    document.querySelector('#mercedes-sprinter-316-cdi-sm-img-1')
+        .addEventListener('click', function () {
+            $('#MercedesSprinter316CdiCarouselIndicators').carousel(1);
+        })
+}
+
+if (document.querySelector('#mercedes-sprinter-316-cdi-sm-img-2')) {
+    document.querySelector('#mercedes-sprinter-316-cdi-sm-img-2')
+        .addEventListener('click', function () {
+            $('#MercedesSprinter316CdiCarouselIndicators').carousel(2);
+        })
+}
+
+if (document.querySelector('#mercedes-sprinter-316-cdi-sm-img-3')) {
+    document.querySelector('#mercedes-sprinter-316-cdi-sm-img-3')
+        .addEventListener('click', function () {
+            $('#MercedesSprinter316CdiCarouselIndicators').carousel(3);
+        })
+}
+
+if (document.querySelector('#mercedes-sprinter-316-cdi-sm-img-4')) {
+    document.querySelector('#mercedes-sprinter-316-cdi-sm-img-4')
+        .addEventListener('click', function () {
+            $('#MercedesSprinter316CdiCarouselIndicators').carousel(4);
+        })
+}
+
+// Set MAN A01 carousel to clicked frame:
+
+if (document.querySelector('#man-a01-sm-img-0')) {
+    document.querySelector('#man-a01-sm-img-0')
+        .addEventListener('click', function () {
+            $('#ManA01CarouselIndicators').carousel(0);
+        })
+}
+
+if (document.querySelector('#man-a01-sm-img-1')) {
+    document.querySelector('#man-a01-sm-img-1')
+        .addEventListener('click', function () {
+            $('#ManA01CarouselIndicators').carousel(1);
+        })
+}
+
+if (document.querySelector('#man-a01-sm-img-2')) {
+    document.querySelector('#man-a01-sm-img-2')
+        .addEventListener('click', function () {
+            $('#ManA01CarouselIndicators').carousel(2);
+        })
+}
+
+if (document.querySelector('#man-a01-sm-img-3')) {
+    document.querySelector('#man-a01-sm-img-3')
+        .addEventListener('click', function () {
+            $('#ManA01CarouselIndicators').carousel(3);
+        })
+}
+
+if (document.querySelector('#man-a01-sm-img-4')) {
+    document.querySelector('#man-a01-sm-img-4')
+        .addEventListener('click', function () {
+            $('#ManA01CarouselIndicators').carousel(4);
+        })
+}
+
+if (document.querySelector('#man-a01-sm-img-5')) {
+    document.querySelector('#man-a01-sm-img-5')
+        .addEventListener('click', function () {
+            $('#ManA01CarouselIndicators').carousel(5);
+        })
+}
+
+if (document.querySelector('#man-a01-sm-img-6')) {
+    document.querySelector('#man-a01-sm-img-6')
+        .addEventListener('click', function () {
+            $('#ManA01CarouselIndicators').carousel(6);
+        })
+}
+
+// Set Renault Ares carousel to clicked frame:
+
+if (document.querySelector('#renault-ares-sm-img-0')) {
+    document.querySelector('#renault-ares-sm-img-0')
+        .addEventListener('click', function () {
+            $('#RenaultAresCarouselIndicators').carousel(0);
+        })
+}
+
+if (document.querySelector('#renault-ares-sm-img-1')) {
+    document.querySelector('#renault-ares-sm-img-1')
+        .addEventListener('click', function () {
+            $('#RenaultAresCarouselIndicators').carousel(1);
+        })
+}
+
+if (document.querySelector('#renault-ares-sm-img-2')) {
+    document.querySelector('#renault-ares-sm-img-2')
+        .addEventListener('click', function () {
+            $('#RenaultAresCarouselIndicators').carousel(2);
+        })
+}
+
+if (document.querySelector('#renault-ares-sm-img-3')) {
+    document.querySelector('#renault-ares-sm-img-3')
+        .addEventListener('click', function () {
+            $('#RenaultAresCarouselIndicators').carousel(3);
+        })
+}
+
+if (document.querySelector('#renault-ares-sm-img-4')) {
+    document.querySelector('#renault-ares-sm-img-4')
+        .addEventListener('click', function () {
+            $('#RenaultAresCarouselIndicators').carousel(4);
+        })
+}
+
+if (document.querySelector('#renault-ares-sm-img-5')) {
+    document.querySelector('#renault-ares-sm-img-5')
+        .addEventListener('click', function () {
+            $('#RenaultAresCarouselIndicators').carousel(5);
+        })
+}
+
+// Set Mercedes Sprinter 413 CDI carousel to clicked frame:
+
+if (document.querySelector('#mercedes-sprinter-413-cdi-sm-img-0')) {
+    document.querySelector('#mercedes-sprinter-413-cdi-sm-img-0')
+        .addEventListener('click', function () {
+            $('#MercedesSprinter413CdiCarouselIndicators').carousel(0);
+        })
+}
+
+if (document.querySelector('#mercedes-sprinter-413-cdi-sm-img-1')) {
+    document.querySelector('#mercedes-sprinter-413-cdi-sm-img-1')
+        .addEventListener('click', function () {
+            $('#MercedesSprinter413CdiCarouselIndicators').carousel(1);
+        })
+}
+
+if (document.querySelector('#mercedes-sprinter-413-cdi-sm-img-2')) {
+    document.querySelector('#mercedes-sprinter-413-cdi-sm-img-2')
+        .addEventListener('click', function () {
+            $('#MercedesSprinter413CdiCarouselIndicators').carousel(2);
+        })
+}
+
+if (document.querySelector('#mercedes-sprinter-413-cdi-sm-img-3')) {
+    document.querySelector('#mercedes-sprinter-413-cdi-sm-img-3')
+        .addEventListener('click', function () {
+            $('#MercedesSprinter413CdiCarouselIndicators').carousel(3);
+        })
+}
+
+if (document.querySelector('#mercedes-sprinter-413-cdi-sm-img-4')) {
+    document.querySelector('#mercedes-sprinter-413-cdi-sm-img-4')
+        .addEventListener('click', function () {
+            $('#MercedesSprinter413CdiCarouselIndicators').carousel(4);
+        })
+}
+
+// Set Neoplan 4016 carousel to clicked frame:
+
+if (document.querySelector('#neoplan-4016-sm-img-0')) {
+    document.querySelector('#neoplan-4016-sm-img-0')
+        .addEventListener('click', function () {
+            $('#Neoplan4016CarouselIndicators').carousel(0);
+        })
+}
+
+if (document.querySelector('#neoplan-4016-sm-img-1')) {
+    document.querySelector('#neoplan-4016-sm-img-1')
+        .addEventListener('click', function () {
+            $('#Neoplan4016CarouselIndicators').carousel(1);
+        })
+}
+
+if (document.querySelector('#neoplan-4016-sm-img-2')) {
+    document.querySelector('#neoplan-4016-sm-img-2')
+        .addEventListener('click', function () {
+            $('#Neoplan4016CarouselIndicators').carousel(2);
+        })
+}
+
+if (document.querySelector('#neoplan-4016-sm-img-3')) {
+    document.querySelector('#neoplan-4016-sm-img-3')
+        .addEventListener('click', function () {
+            $('#Neoplan4016CarouselIndicators').carousel(3);
+        })
+}
+
+if (document.querySelector('#neoplan-4016-sm-img-4')) {
+    document.querySelector('#neoplan-4016-sm-img-4')
+        .addEventListener('click', function () {
+            $('#Neoplan4016CarouselIndicators').carousel(4);
+        })
+}
+
+if (document.querySelector('#neoplan-4016-sm-img-5')) {
+    document.querySelector('#neoplan-4016-sm-img-5')
+        .addEventListener('click', function () {
+            $('#Neoplan4016CarouselIndicators').carousel(5);
+        })
+}
+
+// Set Neoplan 4011 kék carousel to clicked frame:
+
+if (document.querySelector('#neoplan-4011-kek-sm-img-0')) {
+    document.querySelector('#neoplan-4011-kek-sm-img-0')
+        .addEventListener('click', function () {
+            $('#Neoplan4011KekCarouselIndicators').carousel(0);
+        })
+}
+
+if (document.querySelector('#neoplan-4011-kek-sm-img-1')) {
+    document.querySelector('#neoplan-4011-kek-sm-img-1')
+        .addEventListener('click', function () {
+            $('#Neoplan4011KekCarouselIndicators').carousel(1);
+        })
+}
+
+if (document.querySelector('#neoplan-4011-kek-sm-img-2')) {
+    document.querySelector('#neoplan-4011-kek-sm-img-2')
+        .addEventListener('click', function () {
+            $('#Neoplan4011KekCarouselIndicators').carousel(2);
+        })
+}
+
+if (document.querySelector('#neoplan-4011-kek-sm-img-3')) {
+    document.querySelector('#neoplan-4011-kek-sm-img-3')
+        .addEventListener('click', function () {
+            $('#Neoplan4011KekCarouselIndicators').carousel(3);
+        })
+}
+
+if (document.querySelector('#neoplan-4011-kek-sm-img-4')) {
+    document.querySelector('#neoplan-4011-kek-sm-img-4')
+        .addEventListener('click', function () {
+            $('#Neoplan4011KekCarouselIndicators').carousel(4);
+        })
+}
+
+if (document.querySelector('#neoplan-4011-kek-sm-img-5')) {
+    document.querySelector('#neoplan-4011-kek-sm-img-5')
+        .addEventListener('click', function () {
+            $('#Neoplan4011KekCarouselIndicators').carousel(5);
+        })
+}
+
+// Set Neoplan 4011 fehér carousel to clicked frame:
+
+if (document.querySelector('#neoplan-4011-feher-sm-img-0')) {
+    document.querySelector('#neoplan-4011-feher-sm-img-0')
+        .addEventListener('click', function () {
+            $('#Neoplan4011FeherCarouselIndicators').carousel(0);
+        })
+}
+
+if (document.querySelector('#neoplan-4011-feher-sm-img-1')) {
+    document.querySelector('#neoplan-4011-feher-sm-img-1')
+        .addEventListener('click', function () {
+            $('#Neoplan4011FeherCarouselIndicators').carousel(1);
+        })
+}
+
+if (document.querySelector('#neoplan-4011-feher-sm-img-2')) {
+    document.querySelector('#neoplan-4011-feher-sm-img-2')
+        .addEventListener('click', function () {
+            $('#Neoplan4011FeherCarouselIndicators').carousel(2);
+        })
+}
+
+if (document.querySelector('#neoplan-4011-feher-sm-img-3')) {
+    document.querySelector('#neoplan-4011-feher-sm-img-3')
+        .addEventListener('click', function () {
+            $('#Neoplan4011FeherCarouselIndicators').carousel(3);
+        })
+}
+
+if (document.querySelector('#neoplan-4011-feher-sm-img-4')) {
+    document.querySelector('#neoplan-4011-feher-sm-img-4')
+        .addEventListener('click', function () {
+            $('#Neoplan4011FeherCarouselIndicators').carousel(4);
+        })
+}
+
+if (document.querySelector('#neoplan-4011-feher-sm-img-5')) {
+    document.querySelector('#neoplan-4011-feher-sm-img-5')
+        .addEventListener('click', function () {
+            $('#Neoplan4011FeherCarouselIndicators').carousel(5);
+        })
+}
